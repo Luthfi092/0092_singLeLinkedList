8a0b0e4ef566d0b4399bd3c7111879fcd012c88d
upi1682 <upi538287@gmail.com>
2025-05-08 19:31:23 +0700

    singly1.10

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -93,6 +93,20 @@ public:
 
     void traverse()
     {
-
+        if (listEmpty())
+        {
+            cout << "\nList kosong\n";
+        }
+        else
+        {
+            cout << "\nData di dalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
     }
 };
